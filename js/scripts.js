d4c6bb6c2ebf8b4298e1fe66ff37f41650e0d0c0
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-08-11 01:20:34 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello world');
+
+let favoriteFood = 'pizza';
+document.write(favoriteFood);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
