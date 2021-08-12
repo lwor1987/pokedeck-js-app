7ef473f0ae2e6ea72b3025072637b5e43ad0ec23
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-08-12 11:23:24 -0400

    Made changes to script.
    Changed the variable name.
    Added pokemon names to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-let myVariable = 'pokemonList'
-console.log(myVariable)
+let pokemonList = [
+  {name:'Squirtle', height:0.5, types: 'grass','electric'}
+  {name:'Drowzee', height: 1, types: 'bug', 'dragon'}
+  {name:'Vaporeon', height:1, types: 'grass', 'electric'}
+];
+console.log(pokemonList);  
