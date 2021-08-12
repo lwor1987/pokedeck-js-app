705ec02354628ee1c9bbed209181e4b9e941caf0
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-08-12 00:22:25 -0400

    Made changes to js.
    I erased what was already written.
    I added the variable pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,2 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let myVariable = 'pokemonList'
+console.log(myVariable)
