a6287d8b84df1e8414831251b012e25f85dde3ee
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-08-12 17:09:46 -0400

    Wrapped my arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  {name:'Squirtle', height:0.5, types: 'grass','electric'}
-  {name:'Drowzee', height: 1, types: 'bug', 'dragon'}
-  {name:'Vaporeon', height:1, types: 'grass', 'electric'}
+  {name:'Squirtle', height:0.5, types: ['grass','electric']}
+  {name:'Drowzee', height: 1, types: ['bug', 'dragon']}
+  {name:'Vaporeon', height:1, types: ['grass', 'electric']}
 ];
-console.log(pokemonList);  
+console.log(pokemonList);
