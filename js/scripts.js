92dc45782f987f620357e945da5b82ad46723060
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-08-16 23:44:39 -0400

    Fixed my if statement to read correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
 let pokemonList = [
   {
-    name:'Squirtle',
-   height:0.5,
+    name:"Squirtle (height: 0.7)",
+    height: 0.7,
    types: ['grass','electric']
  },
   {
-    name:'Drowzee',
-   height: 1,
+    name:"Drowzee (height: 1)",
+    height: 1,
    types: ['bug','dragon']
  },
   {
-    name:'Pidgeotto',
-   height:3.7,
+    name:"Pidgeotto (height: 3.7)",
+    height: 3.7,
     types: ['normal','flying']
   },
 ];
 console.log(pokemonList);
 
-pokemonList = "Squirtle (height: 0.5)"
-document.write(pokemonList);
-pokemonList = "Drowzee (height: 1)"
-document.write(pokemonList);
-pokemonList = "Pidgeotto (height: 3.7)"
-document.write(pokemonList);
+
 
 
 
 
 for (let i = 0 ; i < pokemonList.length; i++){
 
-  if (pokemonList[i].height >3)
-  document.write('<p>' + pokemonList[i].name +  "Wow that's big!" + '</p>');
+
+  document.write("<p>" + pokemonList[i].name + "</p>");
+  if (pokemonList[i].height > 3)
+  document.write('<p>' + "Wow that's big!" + '</p>');
 }
