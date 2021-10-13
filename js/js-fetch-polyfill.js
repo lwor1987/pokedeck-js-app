efa8a195c091a070e925f2f4fd03884a2d1076e3
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-10-13 15:51:54 -0400

    Delete js-fetch-polyfill.js

--- js/js-fetch-polyfill.js
+++
@@ -1,7 +0,0 @@
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=20').then(function (response) {
-  return response.json(); // This returns a promise!
-}).then(function (pokemonList) {
-  console.log(pokemonList); // The actual JSON response
-}).catch(function () {
-  // Error
-});
