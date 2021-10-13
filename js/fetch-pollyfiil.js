fb2f61ff3fd6d48ff5a4ac600f759a1657efa805
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-10-13 15:52:23 -0400

    Delete fetch-pollyfiil.js

--- js/fetch-pollyfiil.js
+++
@@ -1,8 +0,0 @@
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=150').then
-(function (response) {
-  return response.json();
-}).then(function (pokemonList) {
-  console.log(pokemonList);
-}).catch(function () {
-
-});
